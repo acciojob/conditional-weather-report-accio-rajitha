3d47224712362b65ec22ba5c0bbef6d4a7b14190
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-28 16:59:06 +0530

    code modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
-import './../styles/App.css';
+//App.js
+import React, { useState, useEffect } from 'react';
+import WeatherDisplay from './WeatherDisplay';
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
+
+  useEffect(() => {
+    // Simulate fetching weather data
+    const weatherInput = { temperature: 25, conditions: 'Sunny' };
+    setWeather(weatherInput);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+/*
+import React, { useState, useEffect } from 'react';
+import WeatherDisplay from './WeatherDisplay';
+
+const App = () => {
+ 
+  const [weatherData, setWeatherData] = useState({
+    temperature: 0,
+    conditions: 'Unknown',
+  });
+
+  
+  useEffect(() => {
+   
+    setWeatherData({ temperature: 25, conditions: 'Sunny' });
+  }, []);
+
+  return (
+    <div>
+     
+      <WeatherDisplay weatherData={weatherData} />
+    </div>
+  );
+};
+
+export default App;
+*/
